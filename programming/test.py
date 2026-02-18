9e3211491e98a56ac7d3996f9065e0d94fbd3757
Daniel Rath <daniel.rath@students.uni-freiburg.de>
2026-02-18 15:11:30 +0100

    Deleted unnecessary files

--- programming/test.py
+++
@@ -1,9 +0,0 @@
-from sklearn.datasets import load_digits
-import matplotlib.pyplot as plt
-
-digits = load_digits()
-print(digits.data.shape)
-
-print(digits.images[0])
-plt.matshow(digits.images[0], cmap="gray")
-plt.show()
